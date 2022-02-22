0ec40fc33527f0ee1d201bb4ece38240c310e4f0
Luis Fernando Cordero Torres <98068290+Luifer7@users.noreply.github.com>
2022-02-21 21:23:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,13 +13,17 @@ btnCerrarMenubar.addEventListener('click', CerrarMenuResponsive)
 function menuResponsive() {
     document.getElementById('menu_responsive').classList.toggle('hide');
     document.getElementById('secciones_cards').classList.toggle('ocultar_seccion');
+    document.getElementById('particles-js').classList.toggle('desaparecer_secciones');
+    document.getElementById('juan').classList.toggle('ocultar_seccion');
 }
 
 
 
 function CerrarMenuResponsive() {
     document.getElementById('menu_responsive').classList.toggle('hide');
+    document.getElementById('particles-js').classList.toggle('desaparecer_secciones');
     document.getElementById('secciones_cards').classList.toggle('ocultar_seccion');
+    document.getElementById('juan').classList.toggle('ocultar_seccion');
 }
 
 
